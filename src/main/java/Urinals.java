001b2e141afc0e54de61b210dba8260d304d9374
Sanket Kapse <sanket8397@gmail.com>
2022-10-27 18:23:42 -0700

    Added verifyValidInputString test and function related to that in code.

--- src/main/java/Urinals.java
+++ src/main/java/Urinals.java
@@ -3,12 +3,18 @@ package main.java;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author sanketkapse
  */
 public class Urinals {
 
+    /**
+     * Reads data from file
+     * @param fileName Read from file fileName
+     */
     public void readFile(String fileName){
         if (fileName.equals("")){
             fileName = "src/main/resources/urinal.dat";
@@ -21,6 +27,19 @@ public class Urinals {
             throw new RuntimeException("File does not exist");
         }
     }
+
+    /**
+     * @param s input string
+     * @return True for valid string, False for invalid string
+     */
+    public boolean verifyValidString(String s){
+        Pattern pattern1 = Pattern.compile("^[0-1]{1,20}$");
+        Pattern pattern2 = Pattern.compile("11{1}");
+        Matcher matcher1 = pattern1.matcher(s);
+        Matcher matcher2 = pattern2.matcher(s);
+        return matcher1.find() & !matcher2.find();
+    }
+
     public static void main(String[] args) {
 
     }
--- src/test/java/UrinalsTest.java
+++ src/test/java/UrinalsTest.java
@@ -5,9 +5,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Test cases for Urinals Class
+ * @author sanketkapse
+ */
 class UrinalsTest {
 
     Urinals urinals;
@@ -21,12 +24,26 @@ class UrinalsTest {
     public void whenFileNotExistsThenThrowFileNotFoundException(){
         Throwable thrown = assertThrows(RuntimeException.class,() -> urinals.readFile("resources/dummy.dat"));
         assertEquals("File does not exist", thrown.getMessage());
+        System.out.println("====== Sanket Surendra Kapse == TEST ONE EXECUTED =======");
     }
 
     @Test
     public void verifyUrinalDatFileExists(){
         Assertions.assertDoesNotThrow(() -> urinals.readFile("src/test/resources/urinal.dat"));
+        System.out.println("====== Sanket Surendra Kapse == TEST TWO EXECUTED =======");
     }
 
+    @Test
+    public void verifyValidInputString(){
+        assertFalse(urinals.verifyValidString("1111"));
+        assertTrue(urinals.verifyValidString("10101"));
+        assertTrue(urinals.verifyValidString("1"));
+        assertTrue(urinals.verifyValidString("01010010100101001010"));
+        assertFalse(urinals.verifyValidString("010100101001010010101"));
+        assertFalse(urinals.verifyValidString("10101101011010110101"));
+        assertFalse(urinals.verifyValidString("q101b"));
+        assertFalse(urinals.verifyValidString("12345"));
+        System.out.println("====== Sanket Surendra Kapse == TEST THREE EXECUTED =======");
+    }
 
 }
